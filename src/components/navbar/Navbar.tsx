afe7e77d7a56ac7997ae158008e8fb5cae50c951
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-08-10 01:37:20 +0600

    created the basic navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,32 +1,124 @@
 import { Link } from "react-router-dom"
 import "./Navbar.css"
 
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
+const navigation = [
+  { name: 'Home', href: '/', current: true },
+  { name: 'Category', href: '/category', current: false },
+  { name: 'About', href: '/about', current: false },
+  { name: 'Contact us', href: '/contact-us', current: false },
+]
+
+function classNames(...classes:any[]) {
+  return classes.filter(Boolean).join(' ')
+}
 const Navbar=()=>{
     return (
         <div>
-<header>
-  <nav className=" fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-    <ul className="navigation max-w-[90vw] flex flex-wrap justify-between items-center relative mx-auto py-8">
-      <Link className="logo" to="/">
-        <h3 className="font-bold text-2xl">LOGO</h3>
-      </Link>
-      <input type="checkbox" id="check" />
 
-      <span className="menu flex [&>li]:pl-8 [&>li>a]:text-center [&>li>a]:relative [&>li>a]:transition [&>li>a]:duration-200 [&>li>a]:ease-in-out [&>li>a]:font-medium [&>li>a]:text-lg">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="#">Projects</Link></li>
-        <li><Link to="#">Resources</Link></li>
-        <li><Link to="#">Contact</Link></li>
+<Disclosure as="nav" className="bg-white">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile menu button*/}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block h-6 w-6 group-data-[open]:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden h-6 w-6 group-data-[open]:block" />
+            </DisclosureButton>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            <div className="flex flex-shrink-0 items-center">
+<Link to="/" className="text-yellow-500 text-2xl font-bold">Electon</Link>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    to={item.href}
+                    aria-current={item.current ? 'page' : undefined}
+                    className={classNames(
+                      item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                      'rounded-md px-3 py-2 text-sm font-medium',
+                    )}
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+  <button
+    type="button"
+    className="relative rounded-full bg-black p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+  >
+    <span className="sr-only">View notifications</span>
+    <BellIcon aria-hidden="true" className="h-6 w-6 text-white" />
+  </button>
 
-        <label htmlFor="check" className="close-menu">X</label>
-      </span>
+  {/* Profile dropdown */}
+  <Menu as="div" className="relative ml-3">
+    <div>
+      <MenuButton className="relative flex rounded-full bg-black text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+        <span className="sr-only">Open user menu</span>
+        <img
+          alt=""
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          className="h-8 w-8 rounded-full"
+        />
+      </MenuButton>
+    </div>
+    <MenuItems
+      transition
+      className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-black border border-gray-700 py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+    >
+      <MenuItem>
+        <a href="#" className="block px-4 py-2 text-sm text-white data-[focus]:bg-gray-700">
+          Your Profile
+        </a>
+      </MenuItem>
+      <MenuItem>
+        <a href="#" className="block px-4 py-2 text-sm text-white data-[focus]:bg-gray-700">
+          Settings
+        </a>
+      </MenuItem>
+      <MenuItem>
+        <a href="#" className="block px-4 py-2 text-sm text-white data-[focus]:bg-gray-700">
+          Sign out
+        </a>
+      </MenuItem>
+    </MenuItems>
+  </Menu>
+</div>
 
-      <label htmlFor="check" className="open-menu">Menu</label>
-    </ul>
-  </nav>
-</header>
+        </div>
+      </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? 'page' : undefined}
+              className={classNames(
+                item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                'block rounded-md px-3 py-2 text-base font-medium',
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+
         </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,9 @@
-import Navbar from "../components/navbar/Navbar";
 
 
 const HomePage=()=>{
 
     return(
         <div>
-            {/* <Navbar></Navbar> */}
             <h2 className="font-semibold text-center text-gray-500 text-3xl">This is home page now You have to modify this page</h2>
         </div>
     )
