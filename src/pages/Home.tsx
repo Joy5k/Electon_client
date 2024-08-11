182156e02977ba1eb27716e99788995b84ce1884
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-08-11 14:32:53 +0600

    added the banner shope and view more button and banner text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,17 @@
 const HomePage=()=>{
 
     return(
-        <div>
-            <h2 className="font-semibold text-center text-gray-500 text-3xl">This is home page now You have to modify this page</h2>
+        <div className="">
+          <div className="p-24">
+            <h3 className="text-7xl text-white font-bold flex flex-col gap-3 "><span>Portable</span><span >Speaker</span></h3>
+            <div className="flex gap-3 mt-20">
+            <button className="capitalize text-white bg-primary hover:bg-gray-800 hover:text-primary p-4 rounded-full text-xl font-bold ">Shop Now</button>
+            <button className="capitalize text-white hover:bg-gray-800 hover:text-white p-4 rounded-full text-xl font-bold border">View more</button>
+            </div>
+          </div>
+          <div>
+
+          </div>
         </div>
     )
 }
