9f4effafc32dfc6542b923e6931f4873e748277c
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-10-26 00:04:13 +0600

    added more user socail platforms

--- src/pages/profile/profile.tsx
+++ src/pages/profile/profile.tsx
@@ -70,6 +70,10 @@ function Profile() {
             <input className="mt-1 border border-gray-500 h-8 rounded-lg bg-gray-900" type="text" name="aboutMe" id="" />
             <label className="mt-10" htmlFor="">Facebook</label>
             <input className="mt-1 border border-gray-500 h-8 rounded-lg bg-gray-900" type="text" name="facebook" id="" />
+            <label className="mt-10" htmlFor="">Instagram</label>
+            <input className="mt-1 border border-gray-500 h-8 rounded-lg bg-gray-900" type="text" name="instagram" id="" />
+            <label className="mt-10" htmlFor="">Twitter</label>
+            <input className="mt-1 border border-gray-500 h-8 rounded-lg bg-gray-900" type="text" name="twitter" id="" />
                 </div>
               </div>
             </section>
