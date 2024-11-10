b6d80eae94a6b883477d1a5a2fb35f09f91419ac
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-11-10 12:00:29 +0600

    responsive the advertise section

--- src/components/advertise/Advertise.tsx
+++ src/components/advertise/Advertise.tsx
@@ -3,18 +3,18 @@ import tv from '../../assets/images/tv.png'
 const Advertise=()=>{
 
     return (
-        <div>
-            <div className='flex justify-between p-10'>
-                <div className='w-4/12'>
-                <img src={tv} className='w-full bg-gray-700' alt="Advertise product Image" />
-                </div>
-                <div  className='w-8/12 flex flex-col justify-center items-center bg-gray-700 gap-10 '>
-                    <span className='p-5 text-white bg-primary px-16 text-center font-semibold text-xl'>HD television</span>
-              <h2 className='text-5xl text-white text-center bg-transparent'>Sale up to 50% off
-              </h2>
-              <p className='text-xl font-semibold text-white bg-transparent'>32 inch HD display
-              </p>
-              <button className='capitalize text-white bg-primary p-4 rounded-full text-center px-8 font-semibold hover:bg-gray-400'>Shop now</button>
+        <div className=' border border-black border-b-red-200 '>
+            <div className='flex flex-col md:flex-row lg:flex-row gap-0 justify-evenly p-10 w-full'>
+                    <div className=''>
+                         <img src={tv} className='w-96 bg-transparent' alt="Advertise product Image" />
+                    </div>
+                <div  className=' flex flex-col justify-center items-center bg-transparent gap-10 '>
+                        <span className='p-2 md:p-5 lg:p-5 text-white bg-primary px-3 md:px-16 text-center font-semibold text-xl'>HD television</span>
+                        <h2 className='text-5xl text-white text-center bg-transparent'>Sale up to 50% off
+                         </h2>
+                         <p className='text-xl font-semibold text-white bg-transparent'>32 inch HD display
+                         </p>
+                        <button className='capitalize text-white bg-primary p-3 md:p-4 rounded-full text-center px-8 font-semibold hover:bg-gray-400'>Shop now</button>
                 </div>
             </div>
         </div>
