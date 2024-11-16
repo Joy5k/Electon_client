57f0976074d9c99970abe246b5508958f3b98342
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-11-16 12:32:24 +0600

    modify the upload product form

--- src/components/productDashboard/uploadProduct.tsx
+++ src/components/productDashboard/uploadProduct.tsx
@@ -57,10 +57,11 @@ const ProductUploadForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-lg mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
+    <form onSubmit={handleSubmit} className=" mx-auto p-6 bg-gray-100 rounded-lg shadow-md">
       <h2 className="text-2xl font-semibold text-gray-800 mb-6">Upload Product</h2>
 
-      <div className="mb-4">
+     <div className="flex justify-center flex-wrap gap-4 ">
+     <div className="mb-4 w-full min-w-40">
         <label className="block text-gray-700 mb-2">Title</label>
         <input
           type="text"
@@ -152,6 +153,7 @@ const ProductUploadForm = () => {
           className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
+     </div>
 
       <button
         type="submit"
