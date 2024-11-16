5bcde5a4de635b02721818c59d116e6ac2b24a6f
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-11-16 21:17:00 +0600

    created the image uploader for products

--- src/components/productDashboard/uploadProduct.tsx
+++ src/components/productDashboard/uploadProduct.tsx
@@ -61,6 +61,24 @@ const ProductUploadForm = () => {
       <h2 className="text-2xl font-semibold mb-6 bg-transparent">Upload Product</h2>
 
      <div className="flex justify-start flex-wrap gap-4 ">
+
+     <div className="flex bg-black w-full ">
+    <div className="extraOutline p-4 w-max  m-auto rounded-lg">
+        <div className="file_upload p-5 relative border-4 border-dotted border-gray-300 rounded-lg" style={{width: "450px"}}>
+            <svg className="text-indigo-500 w-24 mx-auto mb-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" /></svg>
+            <div className="input_field flex flex-col w-max mx-auto text-center">
+                <label>
+                    <input className="text-sm cursor-pointer w-36 hidden" type="file" multiple />
+                    <div className="text bg-indigo-600 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-indigo-500">Upload Image</div>
+                </label>
+
+                <div className="title text-indigo-500 uppercase">or drop files here</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
      <div className="mb-4 w-full min-w-40">
         <label className="block  mb-2">Title</label>
         <input
@@ -84,17 +102,8 @@ const ProductUploadForm = () => {
         ></textarea>
       </div>
 
-      <div className="mb-4">
-        <label className="block  mb-2">Image URL</label>
-        <input
-          type="text"
-          name="image"
-          value={product.image}
-          onChange={handleInputChange}
-          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-          required
-        />
-      </div>
+    
+
 
       <div className="mb-4">
         <label className="block  mb-2">Price ($)</label>
@@ -140,19 +149,7 @@ const ProductUploadForm = () => {
         </button>
       </div>
 
-      <div className="mb-4">
-        <label className="block  mb-2">Rating (optional)</label>
-        <input
-          type="number"
-          name="rating"
-          value={product.rating ?? ""}
-          onChange={handleInputChange}
-          min={0}
-          max={5}
-          step={0.1}
-          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
+   
      </div>
 
       <button
