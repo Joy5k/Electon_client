8116177b6d42b591d0b4a7369534bd5c68be53ad
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-11-26 21:20:24 +0600

    handle zero product ui

--- src/pages/Booking.tsx
+++ src/pages/Booking.tsx
@@ -50,128 +50,132 @@ const total = subtotal; // Add tax, shipping, etc. to total if needed
 
   return (
     <div className="container mx-auto p-4 my-10">
+  {
+    products.length>0 ?  <div>
     {
-      isLoading ? <Spinner></Spinner>:  <div className="flex flex-col md:flex-row lg:flex-row sm:space-x-8">
-      {/* Product Booking Table */}
-      <div  className=' overflow-x-auto' >
-        <table className="w-full bg-white border rounded-md ">
-          <thead>
-            <tr>
-              <th className="py-2 px-4  border-b">Product</th>
-              <th className="py-2 px-4 border-b">Price</th>
-              <th className="py-2 px-4 border-b">Quantity</th>
-              <th className="py-2 px-4 border-b">Subtotal</th>
-              <th className="py-2 px-4 border-b">Remove</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product:any, index:number) => (
-              <tr key={index}>
-                <td className="py-2 px-4 border-b">
-                  <div className="flex items-center">
-                    <img src={product.productId?.image} alt={product.title} className="w-16 h-16 object-cover mr-4" />
-                    <div>
-                      <p className="font-bold">{product.productId?.title}</p>
-                      <p className="text-sm text-gray-500">{product.productId?.color}</p>
-                    </div>
-                  </div>
-                </td>
-                <td className="py-2 px-4 border-b">
-                  <div className="flex justify-center items-center">
-                    ${product.productId?.price}
-                  </div>
-                </td>
-                <td className="py-2 px-4 border-b">
-                  <div className="flex justify-center items-center">
-                    <input
-                      type="number"
-                      value={product?.userSelectedQuantity}
-                      onChange={(e) => handleQuantityChange(index, e.target.value)}
-                      className="w-16 px-2 py-1 border rounded"
-                    />
-                  </div>
-                </td>
-                <td className="py-2 px-4 border-b">
-  <div className="flex justify-center items-center">
-    {(product.productId?.price ?? 1) * (Number(product?.userSelectedQuantity) ?? 1)}
-  </div>
-</td>
-
-
-
-                <td className="py-2 px-4 border-b align-middle">
-                  <div className="flex justify-center items-center">
-                    <button onClick={() => handleDelete(product._id)} className="text-red-600">X</button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-            {/* Additional Row for Buttons */}
-            <tr>
-              <td colSpan={5} className="py-4 px-4">
-                <div className="flex justify-between">
-                  <button
-                    onClick={handleContinueShopping}
-                    className="px-4 py-2 bg-blue-500 text-white rounded"
-                  >
-                    Continue Shopping
-                  </button>
-                  <button
-                    onClick={handleClearData}
-                    className="px-4 py-2 bg-red-500 text-white rounded"
-                  >
-                    Clear Data
-                  </button>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      {/* Cart Total Section */}
-      <div className=" w-full md:w-1/3  lg:w-1/3 p-4 mx-auto  mt-5 md:mt-0 lg:mt-0 bg-black border rounded-md">
-        <h2 className="text-lg font-bold mb-4">Cart Total</h2>
-        <div className="mb-4">
-          <div className="flex justify-between mb-2">
-            <span>Subtotal:</span>
-            <span>${subtotal}</span>
-          </div>
-          <div className="flex items-center mb-4">
-            <input
-              type="text"
-              placeholder="Enter coupon"
-              className="w-full px-2 py-1 border rounded-l"
-            />
-            <button onClick={handleApplyCoupon} className="px-4 py-[5px] bg-blue-500 text-white rounded-r">
-              Apply
-            </button>
-          </div>
-          <div className="mb-4">
-            <label htmlFor="country" className="block mb-1">Choose Country:</label>
-            <select
-              id="country"
-              onChange={(e) => setCountry(e.target.value)}
-              className="w-full px-2 py-1 border rounded"
-            >
-              <option value="CA">Canada</option>
-              <option value="US">Bangladesh</option>
-              <option value="US">United States</option>
-              <option value="UK">United Kingdom</option>
-              {/* Add more countries as needed */}
-            </select>
-          </div>
-          <div className="flex justify-between mb-4">
-            <span>Total:</span>
-            <span>${total}</span>
-          </div>
-         <Link to="/checkout">  <button className="w-full px-4 py-2 bg-green-500 text-white rounded">
-            Proceed to Checkout
-          </button></Link>
-        </div>
-      </div>
-    </div>
-    }
+       isLoading ? <Spinner></Spinner>:  <div className="flex flex-col md:flex-row lg:flex-row sm:space-x-8">
+       {/* Product Booking Table */}
+       <div  className=' overflow-x-auto' >
+         <table className="w-full bg-white border rounded-md ">
+           <thead>
+             <tr>
+               <th className="py-2 px-4  border-b">Product</th>
+               <th className="py-2 px-4 border-b">Price</th>
+               <th className="py-2 px-4 border-b">Quantity</th>
+               <th className="py-2 px-4 border-b">Subtotal</th>
+               <th className="py-2 px-4 border-b">Remove</th>
+             </tr>
+           </thead>
+           <tbody>
+             {products.map((product:any, index:number) => (
+               <tr key={index}>
+                 <td className="py-2 px-4 border-b">
+                   <div className="flex items-center">
+                     <img src={product.productId?.image} alt={product.title} className="w-16 h-16 object-cover mr-4" />
+                     <div>
+                       <p className="font-bold">{product.productId?.title}</p>
+                       <p className="text-sm text-gray-500">{product.productId?.color}</p>
+                     </div>
+                   </div>
+                 </td>
+                 <td className="py-2 px-4 border-b">
+                   <div className="flex justify-center items-center">
+                     ${product.productId?.price}
+                   </div>
+                 </td>
+                 <td className="py-2 px-4 border-b">
+                   <div className="flex justify-center items-center">
+                     <input
+                       type="number"
+                       value={product?.userSelectedQuantity}
+                       onChange={(e) => handleQuantityChange(index, e.target.value)}
+                       className="w-16 px-2 py-1 border rounded"
+                     />
+                   </div>
+                 </td>
+                 <td className="py-2 px-4 border-b">
+   <div className="flex justify-center items-center">
+     {(product.productId?.price ?? 1) * (Number(product?.userSelectedQuantity) ?? 1)}
+   </div>
+ </td>
+ 
+ 
+ 
+                 <td className="py-2 px-4 border-b align-middle">
+                   <div className="flex justify-center items-center">
+                     <button onClick={() => handleDelete(product._id)} className="text-red-600">X</button>
+                   </div>
+                 </td>
+               </tr>
+             ))}
+             {/* Additional Row for Buttons */}
+             <tr>
+               <td colSpan={5} className="py-4 px-4">
+                 <div className="flex justify-between">
+                   <button
+                     onClick={handleContinueShopping}
+                     className="px-4 py-2 bg-blue-500 text-white rounded"
+                   >
+                     Continue Shopping
+                   </button>
+                   <button
+                     onClick={handleClearData}
+                     className="px-4 py-2 bg-red-500 text-white rounded"
+                   >
+                     Clear Data
+                   </button>
+                 </div>
+               </td>
+             </tr>
+           </tbody>
+         </table>
+       </div>
+ 
+       {/* Cart Total Section */}
+       <div className=" w-full md:w-1/3  lg:w-1/3 p-4 mx-auto  mt-5 md:mt-0 lg:mt-0 bg-black border rounded-md">
+         <h2 className="text-lg font-bold mb-4">Cart Total</h2>
+         <div className="mb-4">
+           <div className="flex justify-between mb-2">
+             <span>Subtotal:</span>
+             <span>${subtotal}</span>
+           </div>
+           <div className="flex items-center mb-4">
+             <input
+               type="text"
+               placeholder="Enter coupon"
+               className="w-full px-2 py-1 border rounded-l"
+             />
+             <button onClick={handleApplyCoupon} className="px-4 py-[5px] bg-blue-500 text-white rounded-r">
+               Apply
+             </button>
+           </div>
+           <div className="mb-4">
+             <label htmlFor="country" className="block mb-1">Choose Country:</label>
+             <select
+               id="country"
+               onChange={(e) => setCountry(e.target.value)}
+               className="w-full px-2 py-1 border rounded"
+             >
+               <option value="CA">Canada</option>
+               <option value="US">Bangladesh</option>
+               <option value="US">United States</option>
+               <option value="UK">United Kingdom</option>
+               {/* Add more countries as needed */}
+             </select>
+           </div>
+           <div className="flex justify-between mb-4">
+             <span>Total:</span>
+             <span>${total}</span>
+           </div>
+          <Link to="/checkout">  <button className="w-full px-4 py-2 bg-green-500 text-white rounded">
+             Proceed to Checkout
+           </button></Link>
+         </div>
+       </div>
+     </div>
+     }
+    </div>: <p className='text-4xl  text-center font-bold  text-primary h-screen'>Ooops!... no product added yet</p>
+  }
     </div>
   );
 };
--- src/pages/wishlist/Wishlist.tsx
+++ src/pages/wishlist/Wishlist.tsx
@@ -64,99 +64,101 @@ const Wishlist = () => {
   };
   return (
     <div className="container mx-auto p-4 my-10">
-      <div className="flex flex-col md:flex-row lg:flex-row sm:space-x-8">
-        <div className="overflow-x-auto">
-          <table className="w-full bg-white border rounded-md">
-            <thead>
-              <tr>
-                <th className="py-2 border-b">Product</th>
-                <th className="py-2 px-4 border-b">Price</th>
-                <th className="py-2 px-4 border-b">Quantity</th>
-                <th className="py-2 px-4 border-b">Subtotal</th>
-                <th className="py-2 px-4 border-b">Add to cart</th>
-                <th className="py-2 px-4 border-b">Remove</th>
-              </tr>
-            </thead>
-            <tbody>
-              {wishlist.map((product) => (
-                <tr key={product._id}>
-                  <td className="py-2 px-4 border-b w-fit">
-                    <div className="flex">
-                      <img
-                        src={product.image}
-                        alt={product.title}
-                        className="w-16 h-16 object-cover mr-4"
-                      />
-                      <div>
-                        <p className="font-bold">{product.title}</p>
-                        <p className="text-sm text-gray-500">{product.color}</p>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="py-2 border-b">
-                    <div className="flex justify-center items-center">
-                      ${product.price.toFixed(2)}
-                    </div>
-                  </td>
-                  <td className="py-2 px-4 border-b">
-                    <div className="flex justify-center items-center">
-                      <input
-                        type="number"
-                        value={product.quantity}
-                        onChange={(e) =>
-                          handleQuantityChange(product._id, e.target.value)
-                        }
-                        className="w-16 px-2 py-1 border rounded"
-                      />
-                    </div>
-                  </td>
-                  <td className="py-2 px-4 border-b">
-                    <div className="flex justify-center items-center">
-                      ${(product.price * product.quantity).toFixed(2)}
-                    </div>
-                  </td>
-                  <td className="py-2 px-4 border-b align-middle">
-                    <button
-                      onClick={() => handleAddToCart(product._id)}
-                      className="text-emerald-500 hover:underline"
-                    >
-                      Add to cart
-                    </button>
-                  </td>
-                  <td className="py-2 px-4 border-b align-middle">
-                    <button
-                      onClick={() => handleAddToCart(product._id)}
-                      className="text-red-600"
-                    >
-                      X
-                    </button>
-                  </td>
-                </tr>
-              ))}
+     {
+      wishlist.length ?  <div className="flex flex-col md:flex-row lg:flex-row sm:space-x-8">
+      <div className="overflow-x-auto">
+        <table className="w-full bg-white border rounded-md">
+          <thead>
             <tr>
-              <td colSpan={6} className="py-4 px-4">
-                <div className="flex justify-between">
-                  <button
-                    onClick={handleContinueShopping}
-                    className="px-4 py-2 bg-blue-500 text-white rounded"
-                  >
-                    Continue Shopping
-                  </button>
-                  
-                  <button
-                    onClick={handleClearData}
-                    className="px-4 py-2 bg-red-500 text-white rounded"
-                  >
-                    Clear Data
-                  </button>
-                </div>
-              </td>
+              <th className="py-2 border-b">Product</th>
+              <th className="py-2 px-4 border-b">Price</th>
+              <th className="py-2 px-4 border-b">Quantity</th>
+              <th className="py-2 px-4 border-b">Subtotal</th>
+              <th className="py-2 px-4 border-b">Add to cart</th>
+              <th className="py-2 px-4 border-b">Remove</th>
             </tr>
-           
-            </tbody>
-          </table>
-        </div>
+          </thead>
+          <tbody>
+            {wishlist.map((product) => (
+              <tr key={product._id}>
+                <td className="py-2 px-4 border-b w-fit">
+                  <div className="flex">
+                    <img
+                      src={product.image}
+                      alt={product.title}
+                      className="w-16 h-16 object-cover mr-4"
+                    />
+                    <div>
+                      <p className="font-bold">{product.title}</p>
+                      <p className="text-sm text-gray-500">{product.color}</p>
+                    </div>
+                  </div>
+                </td>
+                <td className="py-2 border-b">
+                  <div className="flex justify-center items-center">
+                    ${product.price.toFixed(2)}
+                  </div>
+                </td>
+                <td className="py-2 px-4 border-b">
+                  <div className="flex justify-center items-center">
+                    <input
+                      type="number"
+                      value={product.quantity}
+                      onChange={(e) =>
+                        handleQuantityChange(product._id, e.target.value)
+                      }
+                      className="w-16 px-2 py-1 border rounded"
+                    />
+                  </div>
+                </td>
+                <td className="py-2 px-4 border-b">
+                  <div className="flex justify-center items-center">
+                    ${(product.price * product.quantity).toFixed(2)}
+                  </div>
+                </td>
+                <td className="py-2 px-4 border-b align-middle">
+                  <button
+                    onClick={() => handleAddToCart(product._id)}
+                    className="text-emerald-500 hover:underline"
+                  >
+                    Add to cart
+                  </button>
+                </td>
+                <td className="py-2 px-4 border-b align-middle">
+                  <button
+                    onClick={() => handleAddToCart(product._id)}
+                    className="text-red-600"
+                  >
+                    X
+                  </button>
+                </td>
+              </tr>
+            ))}
+          <tr>
+            <td colSpan={6} className="py-4 px-4">
+              <div className="flex justify-between">
+                <button
+                  onClick={handleContinueShopping}
+                  className="px-4 py-2 bg-blue-500 text-white rounded"
+                >
+                  Continue Shopping
+                </button>
+                
+                <button
+                  onClick={handleClearData}
+                  className="px-4 py-2 bg-red-500 text-white rounded"
+                >
+                  Clear Data
+                </button>
+              </div>
+            </td>
+          </tr>
+         
+          </tbody>
+        </table>
       </div>
+    </div>: <p className='text-4xl  text-center font-bold  text-primary h-screen'>Ooops!... no product added yet</p>
+     }
     </div>
   );
 };
