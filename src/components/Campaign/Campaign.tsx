47dd657fd211c991b1c71efad2a7f7dd239c31d3
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2024-12-26 15:29:21 +0600

    created the coutner part

--- src/components/Campaign/Campaign.tsx
+++ src/components/Campaign/Campaign.tsx
@@ -1,23 +1,35 @@
 import  { useState, useEffect } from 'react';
 
 import { FaShoppingBag } from 'react-icons/fa'
-import router from '../../assets/images/router.jpg'
 import { FaHeart } from 'react-icons/fa6'
 import { useGetDealOfTheDayQuery } from '../../redux/features/offers/offerManagement';
+import { toast } from 'sonner';
 
 const Campaign=()=>{
     const {data}=useGetDealOfTheDayQuery({})
     const products=data?.data;
-    const [time, setTime] = useState(data?.data?.offerStartDate);
     const singleProduct=data?.data[0].productId;
-console.log(products)
-    useEffect(() => {
-      const interval = setInterval(() => {
-        setTime((prevTime) => prevTime - 1);
-      }, 1000);
-  
-      return () => clearInterval(interval);
-    }, []);
+
+    // const initialTime = Math.floor(
+    //     (new Date(products[0].offerStartDate).getTime() - Date.now()) / 1000
+    //   );
+
+    //   const [time, setTime] = useState(initialTime > 0 ? initialTime : 0);
+      const [time, setTime] = useState( 10)
+    
+      useEffect(() => {
+        const interval = setInterval(() => {
+          setTime((prevTime) => {
+            if (prevTime <= 0) {
+              clearInterval(interval);
+              return 0; // Ensure the counter stops at 0
+            }
+            return prevTime - 1;
+          });
+        }, 1000);
+    
+        return () => clearInterval(interval); // Cleanup on component unmount
+      }, []);
 return (
     <div>
         <div className='flex flex-col md:flex-row  justify-between items-center'>
@@ -37,9 +49,13 @@ return (
                 <p className='text-yellow-400 text-lg font-semibold ml-8'>Price:{singleProduct?.price}$</p>
                <div>
                <div className="flex justify-start items-center mt-10 ml-6">
-                 <div className="text-gray-600 text-5xl font-bold ">
-                      End:{Math.floor(time / 60).toString().padStart(2, '0')}:
-                             {(time % 60).toString().padStart(2, '0')}
+                 <div >
+                   {
+                    time ?   <p className="text-gray-600 text-5xl font-bold ">
+                    End:{Math.floor(time / 60).toString().padStart(2, '0')}:
+                    {(time % 60).toString().padStart(2, '0')}
+                    </p> : <p onClick={()=>toast.warning("wait for next round")}  className='text-red-600  font-semibold underline cursor-pointer '>Offer has been closed</p>
+                   }
                     </div>
                     </div>
                </div>
@@ -55,43 +71,40 @@ return (
         </div>
 
            }
-            <div className=' border-gray-800 flex  justify-center'>
-             {
-                
-            products?.map((product,i:number)=>{
-             
-                     <div key={i} className='flex p-4 border border-dashed mb-3 rounded-md border-gray-800'>
-                <img src={product?.productId?.image} className='w-32 h-fit' alt="" />
-                <div className='ml-4 px-6'>
-                    <p className='text-lg text-white font-semibold font-mono'>{product?.productId?.title}</p>
-                    <p className='text-primary font-mono font-semibold'>Price:{product?.productId?.price}$</p>
-                    <div className=' flex justify-evenly  text-xl mt-6 -ml-7 bg-transparent'>
-                       <button className='p-3 rounded-full border hover:bg-primary'>
-                         <FaShoppingBag className='bg-transparent'></FaShoppingBag>
-                        </button>
-                        <button className='p-3 rounded-full border hover:bg-primary'>
-                            <FaHeart className='bg-transparent'></FaHeart>
-                            </button>
-                    </div>
-                </div>
-             </div>
-                 })
-             }
-             {/* <div className='flex p-4 border border-dashed rounded-md border-gray-800'>
-                <img src={router} className='w-32 h-fit' alt="" />
-                <div className='ml-4 p-6'>
-                    <p className='text-lg text-white font-semibold font-mono'>Bluetooth Speaker</p>
-                    <p className='text-primary font-mono font-semibold'>Price:34$</p>
-                    <div className=' flex justify-evenly  text-xl mt-6 -ml-7 bg-transparent'>
-                       <button className='p-3 rounded-full border hover:bg-primary'>
-                         <FaShoppingBag className='bg-transparent'></FaShoppingBag>
-                        </button>
-                        <button className='p-3 rounded-full border hover:bg-primary'>
-                            <FaHeart className='bg-transparent'></FaHeart>
-                            </button>
-                    </div>
-                </div>
-             </div> */}
+            <div className=' border-gray-800 flex  flex-col  justify-center'>
+            {
+  products?.slice(1,3).map((product, i: number) => {
+    return (
+      <div
+        key={i}
+        className="flex p-4 border border-dashed mb-3 rounded-md border-gray-800"
+      >
+        <img
+          src={product?.productId?.image}
+          className="w-32 h-fit"
+          alt=""
+        />
+        <div className="ml-4 px-6">
+          <p className="text-lg text-white font-semibold font-mono">
+            {product?.productId?.title}
+          </p>
+          <p className="text-primary font-mono font-semibold">
+            Price: {product?.productId?.price}$
+          </p>
+          <div className="flex justify-evenly text-xl mt-6 -ml-7 bg-transparent">
+            <button className="p-3 rounded-full border hover:bg-primary">
+              <FaShoppingBag className="bg-transparent"></FaShoppingBag>
+            </button>
+            <button className="p-3 rounded-full border hover:bg-primary">
+              <FaHeart className="bg-transparent"></FaHeart>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  })
+}
+
 
             </div>
         </div>
