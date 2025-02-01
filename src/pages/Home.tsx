684110cb5394fb881fda6b45d6c8772c4522bd54
Mehedi hasan <mmehedihasanjoyv@gmail.com>
2025-02-01 21:50:22 +0600

    dynamic the header section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,9 +10,21 @@ import CustomerReview from '../components/customerReview/CustomerReview';
 import Features from '../components/features/Features';
 import PopularProduct from '../components/popularProduct/PopularProduct';
 import { useGetSingleProductQuery } from '../redux/features/admin/productManagementApi';
+import { useNavigate } from 'react-router-dom';
 const HomePage=()=>{
+      const navigate=useNavigate()
   const {data}=useGetSingleProductQuery("679e3fe387e4bf1f5ea8392e")
-  console.log(data)
+  const product=data?.data;
+  
+  const handleProceedToCheckout = () => {
+    if (product.length === 0) {
+      alert('Please select at least one product to proceed.');
+      return;
+    }
+    const selectedProducts=[product]
+    // Proceed with the selected products (You can pass `selectedProducts` to the next route)
+    navigate('/checkout', { state: { selectedProducts } });
+  };
 
     return(
      <div>
@@ -20,7 +32,7 @@ const HomePage=()=>{
            <div className="xs:p-4 sm:p-4 md:p-20 flex-1 r">
             <h3 className="text-7xl text-white font-bold flex flex-col gap-3 text-center md:text-start"><span>Portable</span><span >Speaker</span></h3>
             <div className="flex gap-3 mt-20 items-center justify-center md:justify-start">
-            <button className="capitalize text-white bg-primary hover:bg-gray-800 hover:text-primary p-4 rounded-full text-xl font-bold ">Shop Now</button>
+            <button onClick={()=>handleProceedToCheckout()} className="capitalize text-white bg-primary hover:bg-gray-800 hover:text-primary p-4 rounded-full text-xl font-bold ">Shop Now</button>
             <button className="capitalize text-white hover:bg-gray-800 hover:text-white p-4 rounded-full text-xl font-bold border">View more</button>
             </div>
           </div>
